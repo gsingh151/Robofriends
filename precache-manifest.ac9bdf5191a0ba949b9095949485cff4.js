035bdf1c3e2baf9d22d6b8dc3e08997062344bb6
gsingh151 <gaganomj.singh@mail.utoronto.ca>
2020-10-12 00:56:12 -0400

    Updates

---
+++ precache-manifest.ac9bdf5191a0ba949b9095949485cff4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e976da45d800da806dab99b1a620be",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a3651c3afffb7e126f37",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9dd413451419aee46829",
+    "url": "/robofriends/static/css/main.87f88a60.chunk.css"
+  },
+  {
+    "revision": "a3651c3afffb7e126f37",
+    "url": "/robofriends/static/js/2.b9daffd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.b9daffd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dd413451419aee46829",
+    "url": "/robofriends/static/js/main.efad7616.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
